727ede8df7ee1a48fae723252962b3227982f906
Mauro Moreno <mmauromoreno1@gmail.com>
2024-06-26 01:14:24 -0300

    Created basic scripts

---
+++ Battle Karts/Assets/Scripts/Car/Weapon/Weapon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private GameObject weaponPrefab;
+    
+
+    public static void WeaponShoot()
+    {
+
+    }
+
+}
