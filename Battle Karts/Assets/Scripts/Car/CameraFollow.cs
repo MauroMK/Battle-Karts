2edae6830aa002409a112400958bcc42a9c95c2e
Mauro Moreno <mmauromoreno1@gmail.com>
2024-07-02 20:22:50 -0300

    Fixed ps1 graphics, skyboxes and camera grabing the texture

--- Battle Karts/Assets/Scripts/Car/CameraFollow.cs
+++ Battle Karts/Assets/Scripts/Car/CameraFollow.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraFollow : MonoBehaviour
 {
@@ -10,9 +11,30 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Vector3 eulerRotation;
     [SerializeField] private float damper;
 
+    public Camera mainCamera;
+    public RenderTexture ps1RenderTexture;
+
     void Start()
     {
         transform.eulerAngles = eulerRotation;
+
+        // Ensure the camera setup on start
+        SetupCamera();
+
+        // Subscribe to the sceneLoaded event
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        // Unsubscribe from the sceneLoaded event
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // Ensure the camera setup after loading a new scene
+        SetupCamera();
     }
 
     void Update()
@@ -23,5 +45,18 @@ public class CameraFollow : MonoBehaviour
         }
 
         transform.position = Vector3.Lerp(transform.position, target.position + offset, damper * Time.deltaTime);
+    }
+
+    private void SetupCamera()
+    {
+        if (mainCamera == null)
+        {
+            mainCamera = Camera.main;
+        }
+
+        if (mainCamera != null && ps1RenderTexture != null)
+        {
+            mainCamera.targetTexture = ps1RenderTexture;
+        }
     }
 }
